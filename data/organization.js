8074946abef93f86f74357913e0b66578b9b7e56
protagonist77 <lintangbirdasaputra@gmail.com>
2022-10-11 00:26:17 +0700

    mengupdate semuanya

--- data/organization.js
+++
@@ -1,9 +0,0 @@
-const organizations = [
-    {
-        name: "Organisasi Mahasiswa Ahli Teknologi Informasi (OmahTI)",
-        date1: "2022-9-27",
-        position: "Back-end",
-        image: "/images/omahti.jpg"
-    }
-]
-export default organizations
