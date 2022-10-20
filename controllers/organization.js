ed8501e96ec616a2568db64bd7a5d2d7baefb746
protagonist77 <lintangbirdasaputra@gmail.com>
2022-10-20 15:59:43 +0700

    menambah file uplload

---
+++ controllers/organization.js
@@ -0,0 +1,131 @@
+import { create } from "mquery/lib/utils.js";
+import { Organizations } from "../models/organizationModel.js";
+
+const getAllOrganizations = async(req, res) => {
+    try{
+        const organizations = await Organizations.find({})
+            .populate("projects", "name description date1 technologies affiliation")
+            .populate("activities", "name description date1 date2")
+            .populate("profile", "name");
+        if (!organizations ) {
+            res.status(404)
+            throw new Error("daftar org tidak ditemukan")
+        }
+        res.json(organizations);
+
+    } catch(error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+const postNewOrganization = async(req, res) => {
+    try {
+        const createdOrganization = new Organizations(req.body);
+        
+        await createdOrganization.save();
+        res.status(201).json(createdOrganization)
+
+    } catch(error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+// adayang lsaah di getorganizationbyid
+const getOrganizationById = async(req, res) => {
+    try{
+        console.log("coba bisa gak ")
+        const organization = await Organizations.findById(req.params.id)
+            .populate("projects", "name description date1 technologies affiliation")
+            .populate("activities", "name description date1 date2")
+            .populate("profile", "name");
+        console.log("kalo keluar berarti bisa querynya")
+        if (!organization) {
+            res.status(404);
+            throw new Error("Organization tidak ditemukan");
+        }
+        return res.json(organization)
+
+
+    } catch(error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+const putOrganizationById = async(req, res) => {
+    const {
+        name,
+        position,
+        date1,
+        date2,
+        image,
+        activities,
+        projects,
+        profile
+    } = req.body;
+    console.log("reqbody: ",req.body)
+
+    const organization = await Organizations.findById(req.params.id);
+    if (!organization) {
+        res.status(404);
+        throw new Error("Org tidak ditemukan");
+    }
+    try {
+        organization.name = name || organization.name;
+        organization.position = position || organization.position;
+        organization.date1 = date1 || organization.date1;
+        organization.date2 = date2 || organization.date2;
+        // if (req.file) {
+        //     organization.image = req.file.path
+        // }else {
+            organization.image = organization.image;
+        // }
+        organization.activities = activities || organization.activities;
+        organization.projects = projects || organization.projects;
+        organization.profile = profile || organization.profile;
+
+        console.log("reqbody: ",req.body)
+        console.log("namey: ",name)
+        console.log("positiony: ",position)
+        console.log("date1y: ",date1)
+        console.log("date2y: ",date2)
+        // console.log("req.file.pathy: ",req.file.path)
+        console.log("namey: ",name)
+        console.log("activitiesy: ",activities)
+        console.log("projectsy: ",projects)
+        console.log("profiley: ",profile)
+
+        await organization.save();
+        console.log("reqbody: ",req.body)
+        res.json(organization);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+
+}
+
+const deleteOrganizationById = async(req, res) => {
+    const organization = await Organizations.findById(req.params.id);
+
+    if (organization) {
+        await organization.remove();
+        res.json({ messsage: "Org dihapus" });
+    } else{
+        res.json(404);
+        throw new Error("Org tak ditemukan")
+    }
+}
+
+export { getAllOrganizations,
+    postNewOrganization,
+    getOrganizationById,
+    deleteOrganizationById,
+    putOrganizationById };
+
+
+
+
+
+
+
+
+
